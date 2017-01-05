c66e004f3802cdcd1fdb5f2aabc025c157b43b29
star318.ss <star318.ss@gmail.com>
2017-01-05 18:22:02 +0900

    first

---
+++ take_script_out.py
@@ -0,0 +1,44 @@
+
+#reading script
+src1s_f=open("C:/Users/14rt0/Documents/English/MENTALIST_script/seazon1/The Mentalist - 1x01 - Pilot.HDTV.0TV.en.srt")
+data_s=src1s_f.readlines()
+
+
+# left starting listnumber and English script
+for d in data_s:
+    if d[0]=="0" or d=="\n" :
+        index_d=data_s.index(d)
+        del data_s[index_d]
+
+
+# remove listnumber's \n
+for dd in data_s:
+    if dd[0].isdigit() and len(dd)<=5:
+        data_num=int(dd,10)
+        data_s[data_s.index(dd)]=str(data_num)+":"
+
+
+
+
+#  list number +script  Remove only script object
+ii=0
+for dd in data_s:
+    if dd[0].isdigit():
+        data_s[ii]=dd+data_s[ii+1]
+        data_s.remove(data_s[ii+1])
+    ii=ii+1
+
+
+
+# if more than 2 gyou  -->one object
+ii=0
+for dd in data_s:
+    if not dd[0].isdigit():
+        data_s[ii-1]=data_s[ii-1]+" "+dd
+        data_s.remove(data_s[ii])
+    ii=ii+1
+print data_s
+# write file
+f=open("C:/Users/14rt0/Desktop/s/script.txt","w")
+f.writelines(data_s)
+f.close()
